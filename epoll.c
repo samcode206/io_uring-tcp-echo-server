e4eed5f06cebf7bc2f608e7b3dba138ea64b9175
samcode206 <hadi.sameem.dev@gmail.com>
2023-08-20 17:29:28 -0700

    license

--- epoll.c
+++ epoll.c
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2023 Sam, H
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 #define _GNU_SOURCE
 #include <assert.h>
 #include <errno.h>
@@ -625,9 +649,7 @@ void server_event_loop_init(server_t *s) {
   }
 }
 
-void sig_int_handler(int sig){ 
-  exit(0);
-}
+void sig_int_handler(int sig) { exit(0); }
 
 int main(void) {
   static_assert((MAX_EVENTS & (MAX_EVENTS - 1)) == 0,
